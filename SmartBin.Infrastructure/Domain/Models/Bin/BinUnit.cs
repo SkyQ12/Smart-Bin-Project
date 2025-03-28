b68c4aa732bb23b886b80b60cae8fe6099ad1134
SkyQ12 <hoangthien12112k5@gmail.com>
2025-03-28 22:58:56 +0700

    Update BinUnit.cs
    Update 28/3

--- SmartBin.Infrastructure/Domain/Models/Bin/BinUnit.cs
+++ SmartBin.Infrastructure/Domain/Models/Bin/BinUnit.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace SmartBin.Infrastructure.Domain.Models.Bin
 {
     public class BinUnit
@@ -7,20 +7,36 @@ namespace SmartBin.Infrastructure.Domain.Models.Bin
         public string BinId { get; set; }
         public BinUnitType Type { get; set; }
         public Bin Bin {  get; set; }
+        public int Fault { get; set; }
+        public int Level { get; set; }
+        public int CompressCnt { get; set; }
+        public int FullCnt { get; set; }
+        public int Status { get; set; }
+        public int Flame { get; set; }
+        public int Vibration { get; set; }
         public List<CollectedHistory> CollectedHistories { get; set; }
         public List<ErrorHistory> ErrorHistories { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public BinUnit() { }
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        public BinUnit(string id, string binId, BinUnitType type, Bin bin, List<CollectedHistory> collectedHistories, List<ErrorHistory> errorHistories)
+
+        public BinUnit(string binUnitId, string binId, BinUnitType type, Bin bin, int fault, int level, int compressCnt, int fullCnt, int status, int flame, int vibration, List<CollectedHistory> collectedHistories, List<ErrorHistory> errorHistories)
         {
-            BinUnitId = id;
+            BinUnitId = binUnitId;
             BinId = binId;
             Type = type;
             Bin = bin;
+            Fault = fault;
+            Level = level;
+            CompressCnt = compressCnt;
+            FullCnt = fullCnt;
+            Status = status;
+            Flame = flame;
+            Vibration = vibration;
             CollectedHistories = collectedHistories;
             ErrorHistories = errorHistories;
         }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
     }
 }
